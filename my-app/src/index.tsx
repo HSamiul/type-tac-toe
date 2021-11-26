afe3b391e866fbb8739427eac752223fbab1dd43
unknown <samiulh@andrew.cmu.edu>
2021-11-25 23:47:47 -0500

    gave all control of squares to board. board has states, which act as props to squares

--- my-app/src/index.tsx
+++ my-app/src/index.tsx
@@ -2,19 +2,57 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-    render() {
+/* make a property (props) and state definition for squares */
+type SquareProps = {
+  value: string|undefined /* a square is either X, O, or empty/undefined */
+  onClick: () => void
+}
+
+type SquareState = {
+  value: string|undefined
+}
+
+class Square extends React.Component<SquareProps, SquareState> { /* Pass in SquareProps for Square objects */
+    render() { /* Returns a JSX element that represents a component of the site */
       return (
-        <button className="square">
-          {/* TODO */}
+        <button 
+          className="square" 
+          onClick={ () => this.props.onClick() }>
+            {this.props.value}
         </button>
       );
     }
   }
-  
-  class Board extends React.Component {
+
+  type BoardProps = {
+    squares: Array<string|undefined> /* a board's squares must be X, O, or empty/undefined */
+  }
+
+  type BoardState = {
+    squares: Array<string|undefined>
+  }
+
+  class Board extends React.Component<BoardProps, BoardState> {
+    constructor (props: BoardProps) {
+      super(props)
+      this.state = {
+        squares: new Array<undefined>(9)
+      }
+    }
+    
+    /* A parent passes its state (mutable) down to its children as a prop (immutable). Board -> square */
+    handleClick(i: number) {
+      const squares = this.state.squares.slice()
+      squares[i] = "X"
+      this.setState({squares: squares})
+    }
+
     renderSquare(i: number) {
-      return <Square />;
+      return (
+        <Square 
+          value={this.state.squares[i]} 
+          onClick={() => this.handleClick(i)} />
+      )
     }
   
     render() {
@@ -48,7 +86,7 @@ class Square extends React.Component {
       return (
         <div className="game">
           <div className="game-board">
-            <Board />
+            <Board squares={new Array<undefined>(9)}/>
           </div>
           <div className="game-info">
             <div>{/* status */}</div>
